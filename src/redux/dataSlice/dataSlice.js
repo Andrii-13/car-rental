050d785f903267d0911c51511aa283edfb3a7a55
Andrii-13 <128071322+Andrii-13@users.noreply.github.com>
2024-02-27 22:57:22 +0200

    Update dataSlice.js

--- src/redux/dataSlice/dataSlice.js
+++ src/redux/dataSlice/dataSlice.js
@@ -9,18 +9,13 @@ const initialState = {
 const dataSlice = createSlice({
   name: 'data',
   initialState,
+
   // reducers: {
   //   addFavorite(state, action) {
   //     console.log(state);
   //     state.adverts = [...state.adverts, action.payload];
   //   },
 
-  //   delFavorite(state, action) {
-  //     state.isfavorite = state.isfavorite.filter(
-  //       item => item.id !== action.payload.id
-  //     );
-  //   },
-  // },
   extraReducers: builder => {
     builder.addCase(fetchAllDataCars.fulfilled, (state, action) => {
       state.adverts = action.payload; //записую в стейт дані зі беку
